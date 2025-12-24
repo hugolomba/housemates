a03b935778c9a084c017cb80921135077e6df522
Hugo Lomba <98366477+hugolomba@users.noreply.github.com>
2025-12-24 17:39:33 +0000

    add join house

--- app/setup-house/join-house.tsx
+++ app/setup-house/join-house.tsx
@@ -4,7 +4,6 @@ import {
   getHouseByInviteCode,
   joinHouseByInviteCode,
 } from "@/lib/actions/house-actions";
-import { House } from "@/prisma/generated/browser";
 import { Prisma } from "@/prisma/generated/client";
 import {
   Avatar,
@@ -16,7 +15,9 @@ import {
   CardHeader,
   Form,
   Input,
+  Spinner,
 } from "@heroui/react";
+import { set } from "better-auth";
 import { useState } from "react";
 
 export default function SetupHouse() {
@@ -25,13 +26,16 @@ export default function SetupHouse() {
   const [houseToJoin, setHouseToJoin] = useState<Prisma.HouseGetPayload<{
     include: { users: true };
   }> | null>(null);
+  const [error, setError] = useState<string | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
 
   const validateCode = async (e: React.FormEvent) => {
     e.preventDefault();
+    setIsLoading(true);
     const house = await getHouseByInviteCode(inviteCode);
     setHouseToJoin(house);
     setCodeIsValid(house !== null);
-    console.log("House to join:", house);
+    setIsLoading(false);
   };
 
   const joinHouse = async () => {
@@ -47,61 +51,67 @@ export default function SetupHouse() {
         code.
       </h2>
 
-      {!codeIsValid ? (
-        <Form
-          className="w-full flex flex-col items-center"
-          onSubmit={validateCode}
-        >
-          <Input
-            label="House Invite Code"
-            placeholder="Enter the house invite code"
-            labelPlacement="outside"
-            required
-            size="lg"
-            onValueChange={setInviteCode}
-          />
-          <Button type="submit" className="mt-4" size="lg" variant="solid">
-            Validate Invite Code{" "}
-          </Button>
-        </Form>
-      ) : (
-        // <C className="w-full flex flex-col items-center">
-        //   <h3 className="text-xl font-semibold mb-4">House Found!</h3>
-        //   <p className="mb-2">House Name: {houseToJoin.name}</p>
-        //   <p className="mb-4">Address: {houseToJoin.address || "N/A"}</p>
-        //   <Button size="lg" variant="solid">
-        //     Join House
-        //   </Button>
-        // </div>
-        <Card className="w-full p-4">
-          {/* <CardHeader>
+      <div className="w-full min-h-[300px] flex items-center justify-center">
+        {isLoading && <Spinner size="lg" variant="wave" />}
+
+        {!codeIsValid && !isLoading && (
+          <Form
+            className="w-full flex flex-col items-center"
+            onSubmit={validateCode}
+          >
+            <Input
+              label="House Invite Code"
+              placeholder="Enter the house invite code"
+              labelPlacement="outside"
+              required
+              size="lg"
+              onValueChange={setInviteCode}
+            />
+            <Button type="submit" className="mt-4" size="lg" variant="solid">
+              Validate Invite Code{" "}
+            </Button>
+          </Form>
+        )}
+
+        {!isLoading && codeIsValid && (
+          // <C className="w-full flex flex-col items-center">
+          //   <h3 className="text-xl font-semibold mb-4">House Found!</h3>
+          //   <p className="mb-2">House Name: {houseToJoin.name}</p>
+          //   <p className="mb-4">Address: {houseToJoin.address || "N/A"}</p>
+          //   <Button size="lg" variant="solid">
+          //     Join House
+          //   </Button>
+          // </div>
+          <Card className="w-full p-4">
+            {/* <CardHeader>
             {houseToJoin.name} - Invite Code: {houseToJoin.inviteCode}
           </CardHeader> */}
-          <CardBody className="flex items-center">
-            <p className="text-2xl font-bold mb-2">{houseToJoin?.name}</p>
-            <p className="mb-4"> {houseToJoin?.address || "N/A"}</p>
-            <p className="mb-4">Members: </p>
-            <AvatarGroup isBordered>
-              {houseToJoin?.users.map(
-                (
-                  user: Prisma.UserGetPayload<{
-                    include: { house: true } | null;
-                  }>
-                ) => (
-                  <Avatar
-                    src={user.image || ""}
-                    name={user.name || "User"}
-                    key={user.id}
-                  />
-                )
-              )}
-            </AvatarGroup>
-          </CardBody>
-          <CardFooter className="flex flex-col items-center">
-            <Button onPress={joinHouse}>Join House</Button>
-          </CardFooter>
-        </Card>
-      )}
+            <CardBody className="flex items-center">
+              <p className="text-2xl font-bold mb-2">{houseToJoin?.name}</p>
+              <p className="mb-4"> {houseToJoin?.address || "N/A"}</p>
+              <p className="mb-4">Members: </p>
+              <AvatarGroup isBordered>
+                {houseToJoin?.users.map(
+                  (
+                    user: Prisma.UserGetPayload<{
+                      include: { house: true } | null;
+                    }>
+                  ) => (
+                    <Avatar
+                      src={user.image || ""}
+                      name={user.name || "User"}
+                      key={user.id}
+                    />
+                  )
+                )}
+              </AvatarGroup>
+            </CardBody>
+            <CardFooter className="flex flex-col items-center">
+              <Button onPress={joinHouse}>Join House</Button>
+            </CardFooter>
+          </Card>
+        )}
+      </div>
     </div>
   );
 }
